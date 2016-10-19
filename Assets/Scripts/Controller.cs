54a318f4f06cf160a0a18a5240322be1de74a741
Carles Rizo <al315540@uji.es>
2016-10-20 01:07:59 +0200

    Coleccionables y canvas empezados
    GITHUB KK

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Controller : MonoBehaviour
+{
+    public Text text;
+    private int Recogidos;
+
+    // Use this for initialization
+    void Start()
+    {
+        Recogidos = 0;
+        text.text = Recogidos.ToString();
+    }
+
+    public void SumaColeccionables()
+    {
+        Recogidos++;
+        text.text = Recogidos.ToString();
+    }
+
+}
