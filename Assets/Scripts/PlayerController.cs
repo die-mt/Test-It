1a76c851af1aa21c0a8130e48184b26c05127ef5
die-mt <al315318@uji.es>
2016-10-20 00:50:38 +0200

    asd

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,10 +7,9 @@ public class PlayerController : MonoBehaviour {
     [HideInInspector]
     public bool jump = false;				// Condition for whether the player should jump.
 
+    public float maxSpeed = 10f;
 
-    public float moveForce = 365f;			// Amount of force added to move the player left and right.
     public float jumpForce = 1000f;			// Amount of force added when the player jumps.
-    public float maxSpeed = 5f;				// The fastest the player can travel in the x axis.
 
     private Transform groundCheck;			// A position marking where to check if the player is grounded.
     private bool grounded = false;			// Whether or not the player is grounded.
@@ -23,7 +22,6 @@ public class PlayerController : MonoBehaviour {
         groundCheck = transform.Find("groundCheck");
         //anim = GetComponent<Animator>();
     }
-
 
     void Update()
     {
@@ -38,48 +36,19 @@ public class PlayerController : MonoBehaviour {
 
     void FixedUpdate()
     {
-        // Cache the horizontal input.
-        float h = Input.GetAxis("Horizontal");
-
-        // The Speed animator parameter is set to the absolute value of the horizontal input.
-        //anim.SetFloat("Speed", Mathf.Abs(h));
+        //anim.SetBool("Ground", grounded);
 
 
-        // If the player is changing direction (h has a different sign to velocity.x) or hasn't reached maxSpeed yet...
-        if (h * GetComponent<Rigidbody2D>().velocity.x < maxSpeed)
-            // ... add a force to the player.
-            GetComponent<Rigidbody2D>().AddForce(Vector2.right * h * moveForce);
+        float move = Input.GetAxisRaw("Horizontal");
 
-        // If the player's horizontal velocity is greater than the maxSpeed...
-        if (Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x) > maxSpeed)
-            // ... set the player's velocity to the maxSpeed in the x axis.
-            GetComponent<Rigidbody2D>().velocity = new Vector2(Mathf.Sign(GetComponent<Rigidbody2D>().velocity.x) * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        //animation.SetFloat("Speed", Mathf.Abs(move));
 
-        // If the input is moving the player right and the player is facing left...
-        if (h > 0 && !facingRight)
-            // ... flip the player.
+        GetComponent<Rigidbody2D>().velocity = new Vector2(move * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+        if (move > 0 && !facingRight)
             Flip();
-        // Otherwise if the input is moving the player left and the player is facing right...
-        else if (h < 0 && facingRight)
-            // ... flip the player.
+        else if (move < 0 && facingRight)
             Flip();
-
-        // If the player should jump...
-        if (jump)
-        {
-            // Set the Jump animator trigger parameter.
-            //anim.SetTrigger("Jump");
-
-            // Play a random jump audio clip.
-            //int i = Random.Range(0, jumpClips.Length);
-            //AudioSource.PlayClipAtPoint(jumpClips[i], transform.position);
-
-            // Add a vertical force to the player.
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce));
-
-            // Make sure the player can't jump again until the jump conditions from Update are satisfied.
-            jump = false;
-        }
     }
 
 
@@ -96,8 +65,8 @@ public class PlayerController : MonoBehaviour {
 
     void OnTriggerStay2D(Collider2D other)
     {
-        if (other.CompareTag("Stairs") /*&& Input.GetButtonDown("Vertical")*/)
-        {
+        //if (other.CompareTag("Stairs") /*&& Input.GetButtonDown("Vertical")*/)
+        /*{
             print("Estoy subiendo");
             float v = Input.GetAxis("Vertical");
 
@@ -108,6 +77,9 @@ public class PlayerController : MonoBehaviour {
             if (Mathf.Abs(GetComponent<Rigidbody2D>().velocity.y) > maxSpeed)
                 // ... set the player's velocity to the maxSpeed in the x axis.
                 GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, Mathf.Sign(GetComponent<Rigidbody2D>().velocity.y) * maxSpeed);
-        }
+        }*/
+
+
+
     }
 }
