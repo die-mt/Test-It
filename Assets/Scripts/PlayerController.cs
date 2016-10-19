e18f1c7e54e13632f2d11e99f38dae8b1159f4d2
die-mt <al315318@uji.es>
2016-10-19 02:57:16 +0200

    Arreglando cosas

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -44,6 +44,7 @@ public class PlayerController : MonoBehaviour {
         // The Speed animator parameter is set to the absolute value of the horizontal input.
         //anim.SetFloat("Speed", Mathf.Abs(h));
 
+
         // If the player is changing direction (h has a different sign to velocity.x) or hasn't reached maxSpeed yet...
         if (h * GetComponent<Rigidbody2D>().velocity.x < maxSpeed)
             // ... add a force to the player.
