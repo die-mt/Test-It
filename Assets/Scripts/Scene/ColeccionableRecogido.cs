8e2a3729e5fc5dc018f846e434ee26464f3d1ab1
die-mt <al315318@uji.es>
2016-10-24 13:26:24 +0200

    Coleccionable arreglado

--- Assets/Scripts/Scene/ColeccionableRecogido.cs
+++ Assets/Scripts/Scene/ColeccionableRecogido.cs
@@ -10,7 +10,7 @@ public class ColeccionableRecogido : MonoBehaviour {
         Controlador = GameObject.Find("Controller");
     }
 
-    void OnTriggerStay2D(Collider2D other)
+    void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
