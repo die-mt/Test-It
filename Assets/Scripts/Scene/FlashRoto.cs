7e87895532780f03af85e03a335d498b9ca55714
Carles Rizo <al315540@uji.es>
2016-11-27 13:27:28 +0100

    flash roto, entra cueva

--- Assets/Scripts/Scene/FlashRoto.cs
+++ Assets/Scripts/Scene/FlashRoto.cs
@@ -3,20 +3,24 @@ using System.Collections;
 
 public class FlashRoto : MonoBehaviour {
 
-    private GameObject Controlador;
-    private GameObject Jugador;
+    private PlayerController Jugador;
+    private GameObject CuerpoJ;
+    private GameObject Plataforma;
 
     void Awake()
     {
-        Controlador = GameObject.Find("Controller");
-        Jugador = GameObject.Find("Player");
+        CuerpoJ= GameObject.Find("Player");
+        Plataforma = GameObject.Find("Platform (7)");
     }
 
-    void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerStay2D(Collider2D other)
     {
-        if (other.tag == "Player" && Jugador.GetComponent<PlayerController>.Flashing)
+        if (other.tag == "Player" && CuerpoJ.GetComponent<PlayerController>().Flashing)// El jugador intenta hacer un salto flash dentro de la zona
         {
-
+            Plataforma.transform.GetChild(0).Translate(0, 1000, 0);//No me deja destruirlo(o no lo se hacer bien) por lo que simplemente lo muevo al quinto carajo
+            CuerpoJ.GetComponent<Rigidbody2D>().velocity = new Vector2(50,-12 );
+            Plataforma = GameObject.Find("Cueva");
+            Plataforma.GetComponent<SpriteRenderer>().sortingLayerName="Props";
         }
     }
 }
