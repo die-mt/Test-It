398abe687663e9d1b8dc2e2eff74ad7c90e59b89
die-mt <al315318@uji.es>
2017-01-02 00:34:10 +0100

    Colliders

--- Assets/Scripts/Scene/DeadLine.cs
+++ Assets/Scripts/Scene/DeadLine.cs
@@ -6,7 +6,7 @@ public class DeadLine : MonoBehaviour {
     void OnTriggerEnter2D(Collider2D other) {
         if (other.CompareTag("Player"))
             //UnityEngine.SceneManagement.SceneManager.LoadScene("Test It! Level1");
-            UnityEngine.SceneManagement.SceneManager.LoadScene("Test It! Level1Rizo");
+            UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);//"Test It! LevelTutorial");
         Destroy(other);
     }
 }
